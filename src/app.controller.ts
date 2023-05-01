481311b719c0652f3c06a50ac6565bbcdb84c622
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-05-01 16:30:54 -0300

    se modificaron controladores

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller("api")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  //Controlador Saludo en Ingles
+  @Get("/saludoIngles")
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  //Controlador Saludo en Español
+  @Get("/saludoEsp")
+  getHola(): string {
+    return this.appService.getHola();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  //Metodo para saludar en Ingles
   getHello(): string {
     return 'Hello World!';
+  }
+
+  //Metodo para saludar en Español
+  getHola(): string {
+    return 'Hola Mundo!';
   }
 }
